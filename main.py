be080bed090516db55ae62296027c6e3b5524979
kunalbh26 <kunalbhardwaj26102002@gmail.com>
2025-07-09 22:34:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.markdown("""
         background: #ffffff;
     }
     .main {
-    background: transparent;
+    background: #000000;
     border-radius: 0px;
     padding: 3rem !important;
     max-width: 640px;
